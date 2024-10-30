9f1d8e25c0c0e4089bb68e4f611896c8c4c53b7a
Yash Solanki <144127816+MagellaX@users.noreply.github.com>
2024-10-31 01:15:49 +0530

    Update test_gpu.py

--- test_gpu.py
+++ test_gpu.py
@@ -1,12 +1,3 @@
 import tensorflow as tf
-
-# Simple GPU check
 print("TensorFlow version:", tf.__version__)
-print("\nGPUs Available:", len(tf.config.list_physical_devices('GPU')))
-print("GPU Device:", tf.test.gpu_device_name())
-
-# Basic GPU test
-if tf.config.list_physical_devices('GPU'):
-    print("\nGPU was found and is ready to use!")
-else:
-    print("\nNo GPU found, using CPU!")
+print("GPU Available:", bool(tf.config.list_physical_devices('GPU')))
