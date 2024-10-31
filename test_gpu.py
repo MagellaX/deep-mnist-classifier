77c283cc55f6197a8a43bb5580819df64dee0881
MagellaX <alphacr792@gmail.com>
2024-10-31 21:14:12 +0530

    updated files

--- test_gpu.py
+++ test_gpu.py
@@ -1,26 +1,12 @@
 import tensorflow as tf
-import numpy as np
 
-# Print TensorFlow version
+# Simple GPU check
 print("TensorFlow version:", tf.__version__)
+print("\nGPUs Available:", len(tf.config.list_physical_devices('GPU')))
+print("GPU Device:", tf.test.gpu_device_name())
 
-# Check GPU availability
-gpus = tf.config.list_physical_devices('GPU')
-print("\nGPU Devices:", gpus)
-
-if gpus:
-    try:
-        # Try a simple GPU operation
-        with tf.device('/GPU:0'):
-            a = tf.random.normal([1000, 1000])
-            b = tf.random.normal([1000, 1000])
-            c = tf.matmul(a, b)
-            print("\nGPU test successful: Matrix multiplication completed")
-            print("Using GPU:", tf.test.gpu_device_name())
-    except RuntimeError as e:
-        print("\nError using GPU:", e)
+# Basic GPU test
+if tf.config.list_physical_devices('GPU'):
+    print("\nGPU was found and is ready to use!")
 else:
-    print("\nNo GPU devices found")
-
-# Additional system info
-print("\nCUDA Available:", tf.test.is_built_with_cuda())
+    print("\nNo GPU found, using CPU!")
