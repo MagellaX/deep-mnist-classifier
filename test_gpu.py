1923404a1efe8b504bc8be120e94baf79796ceb8
Yash Solanki <144127816+MagellaX@users.noreply.github.com>
2024-10-31 21:47:55 +0530

    Update test_gpu.py

--- test_gpu.py
+++ test_gpu.py
@@ -1,10 +1,4 @@
 import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("GPU devices:", tf.config.list_physical_devices('GPU'))
-
-try:
-    with tf.device('/GPU:0'):
-        a = tf.random.normal([1000, 1000])
-        print("\nGPU test successful!")
-except RuntimeError as e:
-    print("\nError using GPU:", e)
+print("TensorFlow:", tf.__version__)
+devices = tf.config.list_physical_devices()
+print("Available devices:", devices)
